a58185a58fa5882873576613b748bd5ed87ed49b
Mustapha SLIMANI <itsmustaphahere@gmail.com>
2023-09-29 13:05:48 +0100

    Submitted Version

---
+++ script.js
@@ -0,0 +1,4 @@
+// ********* CONSTANT AND GLOBAL VARIABLES ********* //
+let yearInput = document.getElementById("year-input");
+let monthInput = document.getElementById("month-input");
+let dayInput = document.getElementById("day-input");
